42ea24993616e61fcece751c3b4aacedc949f9c2
Athanasios Markou <thanasismarko@gmail.com>
2019-04-28 08:56:47 +0100

    feat: Add webpack bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,11 +2,22 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+// Import webpack in order to use it for our dev server
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 // The port that the webserver will listen
 const port = 3000;
 // Create an instance of express
 const app = express();
+// Set up the webpack compiler
+const compiler = webpack(config);
+
+// Tell express to use webpack middleware
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 // Set up the routes that express will handle
 app.get('/', function(request, response) {
